ebd23d1713f346d223e00cabf36669c824133843
dong yuangui <dongyuangui@appadhoc.com>
2019-03-26 18:46:36 +0800

    release Android SDK v5.0.4 $ react-native-adhoc 3.0.1

--- example/android/app/src/main/java/com/appadhoc/react_native/MainApplication.java
+++ example/android/app/src/main/java/com/appadhoc/react_native/MainApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.adhoc.adhocsdk.AdhocTracker;
 import com.adhoc.config.AdhocConfig;
 import com.facebook.react.ReactApplication;
+import com.appadhoc.module.AppadhocPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
-                    new com.appadhoc.module.AppadhocPackage()
+            new AppadhocPackage()
 //                    new AppadhocPackage()
             );
         }
