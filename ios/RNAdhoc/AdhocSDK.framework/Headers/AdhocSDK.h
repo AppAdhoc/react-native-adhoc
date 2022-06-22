af8aa83a3ba7fae3eae7da217b12937f736bc069
Yiming <mayiming@reyun.com>
2022-06-22 17:48:31 +0800

    release iOS 5.2.8

--- ios/RNAdhoc/AdhocSDK.framework/Headers/AdhocSDK.h
+++ ios/RNAdhoc/AdhocSDK.framework/Headers/AdhocSDK.h
@@ -5,7 +5,7 @@
 //  Created by AppAdhoc on 16/10/26.
 //  Copyright © 2016年 AppAdhoc. All rights reserved.
 //
-//  当前 SDK 版本：5.1.7
+//  当前 SDK 版本：5.2.8
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
@@ -16,13 +16,11 @@
  */
 NS_CLASS_AVAILABLE_IOS(8_0) @interface AdhocSDKConfig : NSObject
 @property (nonatomic, copy) NSString *appKey;  /// 官网申请的 key，必填项
-@property (nonatomic, copy) NSString *clientID; /// 自定义 clientid，如无特殊需求，不需要设置
-@property (nonatomic) BOOL enableDebugAssist; /// 是否显示调试按钮，默认为 NO，不显示（若显示，getFlag 与 track 接口访问实时处理，设置的时间间隔无效）
+@property (nonatomic, copy) NSString *clientID;  /// 自定义 clientID，如无特殊需求，不需要设置
+@property (nonatomic) BOOL enableDebugAssist;  /// 是否显示调试按钮，默认为 NO，不显示（若显示，getFlag 与 track 接口访问实时处理，设置的时间间隔无效）
 @property (nonatomic) BOOL crashTrackEnabled;  /// 是否统计 crash 次数，默认为 NO，不进行统计
-@property (nonatomic) BOOL sessionTrackEnabled; /// 是否统计 APP 访问次数，默认为 YES，进行统计
-@property (nonatomic) BOOL durationTrackEnabled; /// 是否统计 APP 一次访问的时长，默认为 YES，进行统计
-@property (nonatomic) BOOL reportImmediatelyEnabled; /// 是否立即上报数据，默认为 NO，不立即上报
-@property (nonatomic) NSTimeInterval backgroundInterval; /// 设置 App 后台允许的最大停留时长，单位秒(s)，在该时间内切换，SDK 认定为同一次访问，默认为 30 分钟(1800s)
+@property (nonatomic) BOOL reportImmediatelyEnabled;  /// 是否立即上报数据，默认为 NO，不立即上报
+@property (nonatomic) BOOL supportBackend;  /// 是否支持后端提供的试验数据
 
 /// 默认配置
 + (id)defaultConfig;
@@ -35,7 +33,7 @@ NS_CLASS_AVAILABLE_IOS(8_0) @interface AdhocSDKConfig : NSObject
 @end
 
 /**
- *  用于获取试验变量以及进行优化指标统计
+ * 用于获取试验变量以及进行优化指标统计
  */
 NS_CLASS_AVAILABLE_IOS(8_0) @interface AdhocSDK : NSObject
 
@@ -44,62 +42,74 @@ NS_CLASS_AVAILABLE_IOS(8_0) @interface AdhocSDK : NSObject
 /// @param options 启动选项
 + (void)startWithConfigure:(AdhocSDKConfig *)config options:(NSDictionary *)options;
 
-/**
- *  获取后台设置的指定的试验变量的值，试验变量的名字注意与后台保持一致
- *  该方法从 SDK 缓存中直接读取，如需同步获取 flag 值，请使用下面的接口
- *
- *  @param flagName     adhoc 后台设置的试验变量名字
- *  @param defaultValue 指定试验变量的默认值
- *
- *  @return 根据后台设置的模块的类型，返回 Objective-C 中对应的类型可能为(NSNumber, NSString)
- */
+/// 获取后台设置的指定的试验变量的值，试验变量的名字注意与后台保持一致 该方法从 SDK 缓存中直接读取，如需同步获取 flag 值，请使用下面的接口
+/// @param flagName     adhoc 后台设置的试验变量名字
+/// @param defaultValue 指定试验变量的默认值
+/// @return 根据后台设置的模块的类型，返回 Objective-C 中对应的类型可能为(NSNumber, NSString)
 + (id)getFlag:(NSString *)flagName default:(id)defaultValue;
 
-/**
- *  异步方式从缓存直接获取试验变量的值，并检查更新本地flags数据
- *
- *  @param flagName     adhoc后台设置的试验变量名字
- *  @param defaultValue 指定试验变量的默认值
- *  @param timeoutInterval 设置此次网络请求的超时时间，单位为秒(s)，默认 30s
- *  @param completionHandler 网络执行结束后的相关操作
- */
+/// 异步方式从缓存直接获取试验变量的值，并检查更新本地flags数据
+/// @param flagName appadhoc 后台设置的试验变量名字
+/// @param defaultValue 指定试验变量的默认值
+/// @param completionHandler 回调操作
++ (void)getFlagFast:(NSString *)flagName
+       defaultValue:(id)defaultValue
+  completionHandler:(void (^)(id flagValue, NSError *error))completionHandler;
+
+/// 异步方式从缓存直接获取试验变量的值，并检查更新本地flags数据
+/// @param flagName appadhoc 后台设置的试验变量名字
+/// @param defaultValue 指定试验变量的默认值
+/// @param timeoutInterval 设置此次网络请求的超时时间，单位为秒(s)，默认 30s
+/// @param completionHandler 回调操作
 + (void)getFlagFast:(NSString *)flagName
                defaultValue:(id)defaultValue
-            timeoutInterval:(NSTimeInterval)timeoutInterval
+    timeoutInterval:(NSTimeInterval)timeoutInterval
           completionHandler:(void (^)(id flagValue, NSError *error))completionHandler;
 
-/**
- *  异步方式从服务器直接获取试验变量的值
- *
- *  @param flagName     adhoc后台设置的试验变量名字
- *  @param defaultValue 指定试验变量的默认值
- *  @param timeoutInterval 设置此次网络请求的超时时间，单位为秒(s)，默认 30s
- *  @param completionHandler 网络执行结束后的相关操作
- */
+/// 异步方式从服务器直接获取试验变量的值
+/// @param flagName appadhoc 后台设置的试验变量名字
+/// @param defaultValue 指定试验变量的默认值
+/// @param completionHandler 回调操作
++ (void)asynchronousGetFlag:(NSString *)flagName
+               defaultValue:(id)defaultValue
+          completionHandler:(void (^)(id flagValue, NSError *error))completionHandler;
+
+/// 异步方式从服务器直接获取试验变量的值
+/// @param flagName appdhoc 后台设置的试验变量名字
+/// @param defaultValue 指定试验变量的默认值
+/// @param timeoutInterval 设置此次网络请求的超时时间，单位为秒(s)，默认 30s
+/// @param completionHandler 回调操作
 + (void)asynchronousGetFlag:(NSString *)flagName
                defaultValue:(id)defaultValue
             timeoutInterval:(NSTimeInterval)timeoutInterval
           completionHandler:(void (^)(id flagValue, NSError *error))completionHandler;
 
-/**
- 统计需要的优化指标，用以实现科学有效的测试
- 
- @param stat_name 后台设置的优化指标，名字须保持一致
- @param stat_value 当前优化指标单次统计的权重
- */
-+ (void)track:(NSString *)stat_name value:(NSNumber *)stat_value;
+/// 统计优化指标
+/// @param key key对应AppAdhoc控制台定义的优化指标名称
+/// @param value 统计数值
++ (void)track:(NSString *)key value:(NSNumber *)value;
+
+/// 统计优化指标
+/// @param key key对应AppAdhoc控制台定义的优化指标名称
+/// @param value 统计数值
+/// @param userAttribute 用户自定义属性
++ (void)track:(NSString *)key value:(NSNumber *)value attribute:(NSDictionary *)userAttribute;
+
+/// 统计优化指标(根据参数tag去重)
+/// @param key key对应AppAdhoc控制台定义的优化指标名称
+/// @param value 统计数值
+/// @param tag 用户定义的标签（用于优化指标上报去重）
++ (void)distinctTrack:(NSString *)key value:(NSNumber *)value tag:(NSString *)tag;
+
+/// 统计优化指标(根据参数tag去重)
+/// @param key key对应AppAdhoc控制台定义的优化指标名称
+/// @param value 统计数值
+/// @param tag 用户定义的标签（用于优化指标上报去重）
+/// @param userAttribute 用户自定义属性
++ (void)distinctTrack:(NSString *)key value:(NSNumber *)value tag:(NSString *)tag attribute:(NSDictionary *)userAttribute;
 
 /**
- 统计需要的优化指标，用以实现科学有效的测试
-
- @param stat_name 后台设置的优化指标，名字须保持一致
- @param stat_value 当前优化指标单次统计的权重
- @param stat_attribute 当前数据的定向条件
- */
-+ (void)track:(NSString *)stat_name value:(NSNumber *)stat_value attribute:(NSDictionary *)stat_attribute;
-
-/**
- *  获取当前设备所在试验的试验名列表
+ *  获取当前设备所在试验的试验列表
  *  1.数组中只有 CONTROL，代表未进入任何试验
  *  2.数组中存在一个或多个字典，代表进入一个或多个试验
  *   {
@@ -110,28 +120,37 @@ NS_CLASS_AVAILABLE_IOS(8_0) @interface AdhocSDK : NSObject
  */
 + (NSArray *)getCurrentExperiments;
 
-/**
- 根据 flag 名称获取该 flag 名称所对应的试验是否加入了试验
- 
- @param flagName flag 名称
- @return 是否加入了试验
- */
+/// 根据试验变量名称获取当前设备所在试验的试验列表
+/// 1.数组中只有 CONTROL，代表未进入任何试验
+/// 2.数组中存在一个或多个字典，代表进入一个或多个试验
+///  {
+///     id = "试验版本 ID";
+///     name = "试验名称";
+///  }
+/// @param flagName 试验变量名称
+/// @return 试验名数组
++ (NSArray *)getCurrentExperimentsByFlagName:(NSString *)flagName;
+
+/// 根据 flag 名称获取该 flag 名称所对应的试验是否加入了试验
+/// @param flagName  flag 名称
+/// @return 是否加入了试验
 + (BOOL)isJoinedExperimentByFlagName:(NSString *)flagName;
 
-/**
- *  获取 ClientID
- *
- *  @return ClientID
- */
+/// 设置 ClientID
+/// @param clientID clientID
++ (void)setClientID:(NSString *)clientID;
+
+/// 获取 ClientID
 + (NSString *)getClientID;
 
-/**
- WKWebView 调用 flag 接口
- 
- @param request 加载链接的request
- @param webView 当前的WKWebView
- */
+/// WKWebView 调用 flag 接口
+/// @param request 加载链接的request
+/// @param webView 当前的WKWebView
 + (BOOL)adhocWKWebViewExecute:(NSURLRequest *)request webView:(WKWebView *)webView;
+
+/// 设置试验数据
+/// @param experimentsArray 试验数据数组
++ (void)setExperiments:(NSArray *)experimentsArray;
 
 @end
 
